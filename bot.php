5375c6497a8539d997a86777531a42eb630bb60c
Magloire07 <magloirekombede796@gmail.com>
2022-08-22 20:08:06 +0200

    commit du 22 août 22

---
+++ bot.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Chatbot in PHP </title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
+</head>
+<body>
+    
+    <div class="wrapper">
+        <div class="title">Simple online chatbot </div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="div icon"><i class="fas fa-user"></i></div>
+                <div class="msg-header">
+                    <p>Hello there how can i help you? </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
